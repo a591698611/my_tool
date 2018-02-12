f2878e11a3babf727c36d7dd61e45017189e12dd
沈彪 <591698611@qq.com>
2018-02-12 20:45:01 +0800

    debounce

--- src/function/index.js
+++ src/function/index.js
@@ -1,6 +1,15 @@
 const isFunction = (obj) => obj instanceof Function || typeof obj === 'function';
 
-const debounce = () => {
+const debounce = (fn, delay = 500) => {
+  let timer = null;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
 };
 
 export {
